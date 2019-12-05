60f3f0c2ef0ab7ac41b424bcc4cae14deb45e804
Ryan Larsen <larsenr12@gmail.com>
2019-12-05 17:24:48 -0500

    fixed classes and javascript

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 //Variables for DOM elements
-const timerEL = document.querySelector('timer')
-const startScreenEL = document.querySelector('startScreen')
-const startBtnEL = document.querySelector('startBtn')
-const questionsEL = document.querySelector('questions')
-const choicesEl = document.querySelector('choices')
-const questionHeadEl = document.querySelector('questionHead')
+const timerEL = document.querySelector('#timer')
+const startScreenEL = document.querySelector('#startScreen')
+const startBtnEL = document.querySelector('#startBtn')
+const questionsEL = document.querySelector('#questions')
+const choicesEl = document.querySelector('#choices')
+const questionHeadEl = document.querySelector('#questionHead')
+const feedbackEl = document.querySelector('#feedback')
 
 
 //Timer
 let time = questions.length * 10
-let timer
+let timer = ''
 
 //Start Screen
 function startQuiz() {
-    startScreenEL.setAttribute('class', 'hide')
-    questionsEL.removeAttribute('class')
+   
+    startScreenEL.setAttribute('class','hidden')
+    questionsEL.removeAttribute('class','hidden')
     timer = setInterval(clockTick, 1000)
     timerEL.textContent = time
     findQuestions()
@@ -22,13 +24,17 @@ function startQuiz() {
 
 //Get Questions from Array
 function findQuestions() {
+    const currentQuestion = questions[currentQuestionIndex]
     questionHeadEl.textContent = currentQuestion.title
     choicesEl.innerHTML = ''
 
-    currentQuestion.choices.forEach(function(choice, i){
-        
+
+    currentQuestion.choices.forEach(function (choice, i) {
+        const choiceButton = document.createElement('button')
+        choiceButton.setAttribute('id', 'choices')
+        choiceButton.setAttribute('value', choice)
+        choiceButton.textContent = i + 1 + '.' + choice
+        choiceButton.onclick = questionClick
+        choicesEl.appendChild(ChoiceButton)
     })
-
-    var currentQuestion = questions[currentQuestionIndex]
-
 }
