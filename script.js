1d62e33e25d13c5dae1e5af9221f3e9f16ef6dc2
Ryan Larsen <larsenr12@gmail.com>
2019-12-05 21:02:45 -0500

    Reworking code for choicesgit

--- script.js
+++ script.js
@@ -4,9 +4,45 @@ const startScreenEL = document.querySelector('#startScreen')
 const startBtnEL = document.querySelector('#startBtn')
 const questionsEL = document.querySelector('#questions')
 const choicesEl = document.querySelector('#choices')
-const questionHeadEl = document.querySelector('#questionHead')
+const titleEl = document.querySelector('#title')
 const feedbackEl = document.querySelector('#feedback')
+let currentQuestionIndex = 0
 
+var questions = [
+    {
+      title: "What is the best kind of dog?",
+      choices: ["Small dogs", "Big Dogs", "All Dogs", "No dogs"],
+      answer: "Big Dogs"
+    },
+    {
+      title: "Who's a good boy?",
+      choices: ["I am", "You are", "He is", "She is"],
+      answer: "You are"
+    },
+    {
+      title: "What is a dog's favorite treat?",
+      choices: [
+        "bacon",
+        "eggs",
+        "ice cream",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "How many dog's should I own?",
+      choices: ["1", "2", "3", "All of them"],
+      answer: "All of them"
+    },
+    {
+      title:
+        "This whole quiz is my opinion it seems like right?",
+      choices: ["Probably", "You made it not me", "Who's you in this instance", "Customer is always right"],
+      answer: "Probably"
+    }
+  ];
+  
 
 //Timer
 let time = questions.length * 10
@@ -24,9 +60,9 @@ function startQuiz() {
 
 //Get Questions from Array
 function findQuestions() {
-    const currentQuestion = questions[currentQuestionIndex]
-    questionHeadEl.textContent = currentQuestion.title
-    choicesEl.innerHTML = ''
+    let currentQuestion = questions[currentQuestionIndex]
+    titleEl.textContent = currentQuestion.title
+    choicesEl.textContent = ''
 
 
     currentQuestion.choices.forEach(function (choice, i) {
@@ -38,3 +74,16 @@ function findQuestions() {
         choicesEl.appendChild(ChoiceButton)
     })
 }
+
+function clockTick() {
+    // update time
+    time--;
+    timerEl.textContent = time;
+  
+    // check if user ran out of time
+    if (time <= 0) {
+      quizEnd();
+    }
+  }
+
+startBtnEL.onclick = startQuiz
